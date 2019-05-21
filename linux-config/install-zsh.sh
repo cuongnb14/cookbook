1f929fed023e81211d922120a057640b39e7c434
Nguyễn Bá Cường <cuongnb14@gmail.com>
2019-05-21 11:03:12 +0700

    Update install-zsh.sh

--- linux-config/install-zsh.sh
+++ linux-config/install-zsh.sh
@@ -2,6 +2,7 @@
 # sudo apt-get install -y zsh
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+sudo apt update
 
 # change theme and disable auto title
 echo "change theme and disable auto title" 
